c745295e96a33795b1632f4cb414be319a7a6257
lhuzarski1 <lukaszhuzarski1@gmail.com>
2022-11-17 14:39:31 +0100

    add/edit course forms

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,19 +3,18 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const activeStyle = { color: "#F15B2A" };
-
   return (
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>
         Home
       </NavLink>
       {" | "}
-      <NavLink to="/about" activeStyle={activeStyle}>
-        About
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
       </NavLink>
       {" | "}
-      <NavLink to="/courses" activeStyle={activeStyle} exact>
-        Courses
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
       </NavLink>
     </nav>
   );
